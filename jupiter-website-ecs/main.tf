babf7ccfc7fe21ee9f95c70aeb012392963ffa73
madebambo <modupe_adeoye@yahoo.com>
2023-08-28 11:38:57 -0400

    created iam role

--- jupiter-website-ecs/main.tf
+++ jupiter-website-ecs/main.tf
@@ -36,3 +36,9 @@ module "nat_gateway" {
     source = "../modules/security-groups"
     vpc_id = module.vpc.vpc_id
   }
+
+    # create security group
+  module "ecs-task-execution-role" {
+    source       = "../modules/ecs-task-execution-role"
+    project_name = module.vpc.project_name
+  }
