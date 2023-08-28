ad0087be8c2e2cbb8d2959e0cb944b9097611175
madebambo <modupe_adeoye@yahoo.com>
2023-08-28 14:51:52 -0400

    push alb files

--- jupiter-website-ecs/main.tf
+++ jupiter-website-ecs/main.tf
@@ -50,3 +50,15 @@ module "nat_gateway" {
     alternative_name  = var.alternative_name
   }
 
+  #create alb module
+  module "application_load_balancer" {
+    source                = "../modules/alb"
+    project_name          = module.vpc.project_name
+    alb_security_group_id = module.security_group.alb_security_group_id
+    public_subnet_az1_id  = module.vpc.public_subnet_az1_id
+    public_subnet_az2_id  = module.vpc.public_subnet_az2_id
+    vpc_id                = module.vpc.vpc_id
+    certificate_arn       = module.acm.certificate_arn
+
+  }
+
