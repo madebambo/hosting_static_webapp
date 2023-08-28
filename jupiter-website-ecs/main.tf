fdec686e02977d94f082a7d1e64e981a1aeabd6f
madebambo <modupe_adeoye@yahoo.com>
2023-08-28 13:10:12 -0400

    created acm files

--- jupiter-website-ecs/main.tf
+++ jupiter-website-ecs/main.tf
@@ -42,3 +42,11 @@ module "nat_gateway" {
     source       = "../modules/ecs-task-execution-role"
     project_name = module.vpc.project_name
   }
+
+  # create acm
+  module "acm" {
+    source            = "../modules/acm"
+    domain_name       = var.domain_name
+    alternative_name  = var.alternative_name
+  }
+
--- jupiter-website-ecs/variables.tf
+++ jupiter-website-ecs/variables.tf
@@ -6,4 +6,7 @@ variable "public_subnet_az2_cidr" {}
 variable "private_app_subnet_az1_cidr" {}
 variable "private_app_subnet_az2_cidr" {}
 variable "private_data_subnet_az1_cidr" {}
-variable "private_data_subnet_az2_cidr" {} 
+variable "private_data_subnet_az2_cidr" {}
+
+variable "domain_name" {}
+variable "alternative_name" {}
